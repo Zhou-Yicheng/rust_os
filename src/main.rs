b0781dc434df5f813ef2ace9fcd0fbcc97026b0e
Zhou-Yicheng <chen@LAPTOP-A3B1TOLG.localdomain>
2021-09-17 13:30:05 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use blog_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello world{}", "!");
+
+    blog_os::init();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It dit not crash");
+    blog_os::hlt_loop();
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    blog_os::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
